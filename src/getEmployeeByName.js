a1c8f83a62259dba3510e7d25589ae875cd4aa35
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-08 15:52:45 -0300

    feat: completed the function that searches employee by name or lastname

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,6 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => (!employeeName ? {} : employees
+  .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName));
 
 module.exports = getEmployeeByName;
