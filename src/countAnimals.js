76cb7261f5a5878e729bbafb2d9c9533a0f27b38
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-10 01:25:21 -0300

    feat: completed the function that counts how many animals per species and sex | requisite 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((count, specie) => {
+      const obj = count;
+      obj[specie.name] = specie.residents.length;
+      return obj;
+    }, {});
+  }
+  if (animal.sex) {
+    const findSpecie = species.find((specie) => specie.name === animal.specie);
+    const residentsBySex = findSpecie.residents.filter((resident) => resident.sex === animal.sex);
+    return residentsBySex.length;
+  }
+  let count = 0;
+  species.forEach((specie) => {
+    if (specie.name === animal.specie) count = specie.residents.length;
+  });
+  return count;
 }
 
 module.exports = countAnimals;
