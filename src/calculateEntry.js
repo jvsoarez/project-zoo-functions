5f18bc40a8252b4efedca4907eab06bb07be609d
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-10 15:38:07 -0300

    feat: completed the function that returns the total entry price | requisite 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const object = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      object.child += 1;
+    } else if ((entrant.age >= 18 && entrant.age < 50)) {
+      object.adult += 1;
+    } else {
+      object.senior += 1;
+    }
+  });
+  return object;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const numberOfEntrants = countEntrants(entrants);
+  const chidValue = numberOfEntrants.child * prices.child;
+  const adultValue = numberOfEntrants.adult * prices.adult;
+  const seniorValue = numberOfEntrants.senior * prices.senior;
+  const totalValue = chidValue + adultValue + seniorValue;
+  return totalValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
