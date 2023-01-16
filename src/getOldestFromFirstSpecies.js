d9b75a0758afd937ea586cf2d71b51234256511d
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-16 20:32:23 -0300

    feat: completed the function that finds information about oldest animal of the first specie managed by param employee | requisite 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idFirtsSpecieByEmployee = employees
+    .find((employee) => employee.id === id).responsibleFor[0];
+  const residentsOfSpecie = species
+    .filter((specie) => specie.id === idFirtsSpecieByEmployee)[0].residents;
+  const oldestAnimal = residentsOfSpecie
+    .reduce((bigger, animal) => (animal.age > bigger.age ? animal : bigger));
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
